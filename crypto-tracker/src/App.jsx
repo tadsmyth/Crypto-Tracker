d6e354e3668d85f6b5bcba77c0eb4d95113b90f2
tadsmyth <tadsmith7@gmail.com>
2021-11-30 18:36:20 -0600

    wired up two pages to the App js

--- crypto-tracker/src/App.jsx
+++ crypto-tracker/src/App.jsx
@@ -1,8 +1,4 @@
 import React, { useState, useEffect}from 'react';
-import CoinList from './components/CoinList';
-import Search from './components/Search';
-import Chart from './components/Chart';
-import Header from './components/Header';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -10,10 +6,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App(props) {
     return (
         <div>
-            <Header />
-            <Route exact path="/details" />
+            <Route exact path="/details" component={Details}/>
             <Route exact path="/src/components/" component={Home} />
-            <Chart />
         </div>
     );
 }
