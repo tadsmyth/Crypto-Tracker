625b250efea74eb8460d76dcdb8890e0a8b287ea
tadsmyth <tadsmith7@gmail.com>
2021-12-01 18:00:13 -0600

    created axios object for api

---
+++ crypto-tracker/src/api/coinGecko.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: "https://api.coingecko.com/api/v3"
+})
+
+
+// Axios is a tool for handeling https requests and making it easier to do APIs.
+//This axios object is storing the baseURL for my call and will make refrenceing it later for different endpopints easier.
--- crypto-tracker/src/components/CoinList.jsx
+++ crypto-tracker/src/components/CoinList.jsx
@@ -1,12 +1,27 @@
 import React , { useEffect , useState }from 'react';
+import coinGecko from '../api/coinGecko';
 
 
 function CoinList(props) {
    // This state will store the data from the API
     const [coins, setCoins] = useState([])
     
-    
-    
+
+    //This effect is the fetch call and uses my previous axios object to grab a new endpoint.
+    useEffect(() => {
+        const fetchData = async () => {
+           const response = await coinGecko.get('/coins/markets', {
+                params: {
+                    vs_currency: 'usd',
+                    ids: 'bitcoin,ethereum'
+                }
+            })
+
+            console.log(response.data)
+        }
+        fetchData()
+        //This empty array keeps the API from fetching again whenever anything at all changes. I think.
+    }, [] )
     
    
     return (
