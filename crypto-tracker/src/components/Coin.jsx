a4d470e98cb4a1763c1f296b367096ec80cff942
tadsmyth <tadsmith7@gmail.com>
2021-12-02 19:18:36 -0600

    style adjustments

--- crypto-tracker/src/components/Coin.jsx
+++ crypto-tracker/src/components/Coin.jsx
@@ -6,14 +6,14 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 function Coin({coin}) {
     return (
             <Link to='/details'>
-                <li className='coinlist-item list-group-item-dark list-group-item-action d-flex justify-content-between align-items-center text-light m-1'>
-                <img className='coinlist-image' src={coin.image} alt="crypto-token-icon" />
+                 <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
+                 <img className="coinlist-image" src={coin.image} alt="" />
                 {/* Below will show the current price inline with each item */}
                 <span className='text-decoration-none'>${coin.current_price}</span>
                 {/* Below is for the price change in 24hr and it will be red or green */}
                 {/* put the fontAwesome arrow icons here */}
-                <span className='text-success m-5'>{coin.price_change_percentage_24h}
-                <FontAwesomeIcon icon={faArrowDown}/>
+                <span className='text-success m-5'><FontAwesomeIcon className='m-1' icon={faArrowDown}/>{coin.price_change_percentage_24h}
+                
                 </span>
 
                 </li>
